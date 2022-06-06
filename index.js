fd58b779798487e348bab54229d281cd7cf54db8
Iury1978 <advent777@yandex.ru>
2022-06-06 12:44:39 +0300

    created modules and class Game from object game

--- index.js
+++ index.js
@@ -1,159 +1,11 @@
+import { Game } from "./modules/game.js";
+
+const game = new Game();
+console.log("game:", game);
 const SIZE_BLOCK = 20;
+const COLUMNS = 10;
+const ROWS = 20;
 
-//  механика игры
-
-const game = {
-  area: [
-    ["o", "o", "o", "o", "o", "o", "o", "o", "o", "o"],
-    ["o", "o", "o", "o", "o", "o", "o", "o", "o", "o"],
-    ["o", "o", "o", "o", "o", "o", "o", "o", "o", "o"],
-    ["o", "o", "o", "o", "o", "o", "o", "o", "o", "o"],
-    ["o", "o", "o", "o", "o", "o", "o", "o", "o", "o"],
-    ["o", "o", "o", "o", "o", "o", "o", "o", "o", "o"],
-    ["o", "o", "o", "o", "o", "o", "o", "o", "o", "o"],
-    ["o", "o", "o", "o", "o", "o", "o", "o", "o", "o"],
-    ["o", "o", "o", "o", "o", "o", "o", "o", "o", "o"],
-    ["o", "o", "o", "o", "o", "o", "o", "o", "o", "o"],
-    ["o", "o", "o", "o", "o", "o", "o", "o", "o", "o"],
-    ["o", "o", "o", "o", "o", "o", "o", "o", "o", "o"],
-    ["o", "o", "o", "o", "o", "o", "o", "o", "o", "o"],
-    ["o", "o", "o", "o", "o", "o", "o", "o", "o", "o"],
-    ["o", "o", "o", "o", "o", "o", "o", "o", "o", "o"],
-    ["o", "o", "o", "o", "o", "o", "o", "o", "o", "o"],
-    ["o", "o", "o", "o", "x", "o", "o", "o", "0", "o"],
-    ["o", "o", "o", "o", "x", "x", "o", "o", "o", "o"],
-    ["o", "o", "o", "o", "o", "x", "o", "o", "o", "o"],
-    ["o", "o", "o", "o", "o", "o", "o", "o", "o", "o"],
-  ],
-
-  activeTetromino: {
-    x: 3,
-    y: 0,
-    block: [
-      ["o", "x", "o"],
-      ["o", "x", "o"],
-      ["x", "x", "o"],
-    ],
-    rotationIndex: 0,
-    rotation: [
-      [
-        ["o", "x", "o"],
-        ["o", "x", "o"],
-        ["x", "x", "o"],
-      ],
-      [
-        ["x", "o", "o"],
-        ["x", "x", "x"],
-        ["o", "o", "o"],
-      ],
-      [
-        ["o", "x", "x"],
-        ["o", "x", "o"],
-        ["o", "x", "o"],
-      ],
-      [
-        ["o", "o", "o"],
-        ["x", "x", "x"],
-        ["o", "o", "x"],
-      ],
-    ],
-  },
-
-  moveLeft() {
-    if (
-      this.checkOutPosition(this.activeTetromino.x - 1, this.activeTetromino.y)
-    ) {
-      this.activeTetromino.x -= 1;
-    }
-  },
-
-  moveRight() {
-    if (
-      this.checkOutPosition(this.activeTetromino.x + 1, this.activeTetromino.y)
-    ) {
-      this.activeTetromino.x += 1;
-    }
-  },
-
-  moveDown() {
-    if (
-      this.checkOutPosition(this.activeTetromino.x, this.activeTetromino.y + 1)
-    ) {
-      this.activeTetromino.y += 1;
-    } else {
-      this.stopMove();
-    }
-  },
-
-  rotateTetromino() {
-    this.activeTetromino.rotationIndex =
-      this.activeTetromino.rotationIndex < 3
-        ? this.activeTetromino.rotationIndex + 1
-        : 0;
-
-    this.activeTetromino.block =
-      this.activeTetromino.rotation[this.activeTetromino.rotationIndex];
-
-    if (
-      !this.checkOutPosition(this.activeTetromino.x, this.activeTetromino.y)
-    ) {
-      this.activeTetromino.rotationIndex =
-        this.activeTetromino.rotationIndex > 0
-          ? this.activeTetromino.rotationIndex - 1
-          : 3;
-
-      this.activeTetromino.block =
-        this.activeTetromino.rotation[this.activeTetromino.rotationIndex];
-    }
-  },
-
-  get viewArea() {
-    const area = JSON.parse(JSON.stringify(this.area));
-    const { x, y, block } = this.activeTetromino;
-    for (let i = 0; i < block.length; i++) {
-      const row = block[i];
-
-      for (let j = 0; j < row.length; j++) {
-        if (row[j] === "x") {
-          area[y + i][x + j] = block[i][j];
-        }
-      }
-    }
-    return area;
-  },
-  // проверка на выход фигуры из игровоф зоны
-  checkOutPosition(x, y) {
-    const tetromino = this.activeTetromino.block;
-
-    for (let i = 0; i < tetromino.length; i++) {
-      for (let j = 0; j < tetromino[i].length; j++) {
-        if (tetromino[i][j] === "o") continue;
-
-        if (
-          !this.area[y + i] ||
-          !this.area[y + i][x + j] ||
-          this.area[y + i][x + j] === "x"
-        ) {
-          return false;
-        }
-      }
-    }
-    return true;
-  },
-  stopMove() {
-    const { x, y, block } = this.activeTetromino;
-
-    for (let i = 0; i < block.length; i++) {
-      const row = block[i];
-
-      for (let j = 0; j < row.length; j++) {
-        if (row[j] === "x") {
-          this.area[y + i][x + j] = block[i][j];
-        }
-      }
-    }
-  },
-};
 // game.viewArea();
 
 //  отрисовка
@@ -163,8 +15,8 @@ const canvas = document.createElement("canvas");
 container.append(canvas);
 // в тетрисе 10 колонок и 20 строк, то есть размер 2 к 1
 canvas.classList.add("game-area");
-canvas.width = SIZE_BLOCK * 10;
-canvas.height = SIZE_BLOCK * 20;
+canvas.width = SIZE_BLOCK * COLUMNS;
+canvas.height = SIZE_BLOCK * ROWS;
 
 const context = canvas.getContext("2d");
 //  функция, отвечающая за отрисовку в поле
@@ -176,7 +28,7 @@ const showArea = (area) => {
 
     for (let x = 0; x < line.length; x++) {
       const block = line[x];
-      if (block === "x") {
+      if (block !== "o") {
         context.fillStyle = "tomato";
         context.strokeStyle = "white";
         context.fillRect(
